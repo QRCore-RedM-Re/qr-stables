6f1cc1e6a64c24c18db6daa3d38ed036de3eb791
xThrasherrr <thrashergamezz@gmail.com>
2023-08-06 16:45:43 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,13 @@
 fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
 
 description 'qr-stables'
 version '1.0.0'
 
-shared_scripts {
-    'shared/config.lua',
-}
-
+shared_scripts { '@ox_lib/init.lua', 'shared/config.lua', }
 client_script 'client/main.lua'
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server/main.lua' }
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
-}
-
-dependencies {
-    'qr-core'
-}
+dependencies { 'qr-core' }
